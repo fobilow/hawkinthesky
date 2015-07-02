a585daf27c41d66332f2640a31682514fba8f1b9
Luke Rodham <luke@snowyapp.com>
2015-07-02 14:07:29 +0100

    Updated theme

--- public/dashboard.php
+++ public/dashboard.php
@@ -7,37 +7,7 @@
         content="width=device-width, initial-scale=1, maximum-scale=1">
   <meta http-equiv="refresh" content="60">
   <link rel="stylesheet" href="/assets/css/bootstrap.min.css"/>
-  <style>
-    body{
-      background-color: #fff;
-      color:#000;
-      font-size:40px;
-    }
-
-    html, body {
-      height: 100%;
-    }
-
-    .container-fluid {
-      min-height: 100%;
-      height: auto !important; /* This line and the next line are not necessary unless you need IE6 support */
-      height: 100%;
-      margin: 0 auto -30px; /* the bottom margin is the negative value of the footer's height */
-    }
-    .footer, .push {
-      font-size:12px;
-      height: 30px; /* .push must be the same height as .footer */
-    }
-
-    .infobar
-    {
-      background-color:#000;
-      padding:5px;
-      font-size:12px;
-      color:#fff;
-      text-align:right;
-    }
-  </style>
+  <link rel="stylesheet" href="/assets/css/theme.css"/>
 </head>
 <body>
 <div class="infobar"><?= date('d M Y H:i A') ?></div>
@@ -48,21 +18,23 @@
       <?php foreach($row as $stats): ?>
       <div class="col-lg-3 col-md-3" style="border:1px solid #ddd;">
         <?php if($stats != null): $metrics = current($stats); ?>
-          <p class="text-primary" style="font-size:30px; margin-bottom:20px;"><?= key($stats) ?></p>
-          <p style="line-height: 30px; margin-bottom:40px;"><?= $metrics['ga:sessions']; ?>
-            <br/><span style="font-size:20px;"><?= Hawk::metricsName('ga:sessions') ?></span>
+          <h2 class="text-primary"><?= key($stats) ?></h2>
+          <hr>
+          
+          <p class="stats-heading"><?= $metrics['ga:sessions']; ?>
+            <br/><span class="stats-details"><?= Hawk::metricsName('ga:sessions') ?></span>
           </p>
 
-          <p style="line-height: 30px; margin-bottom:40px;"><?= $metrics['ga:pageviews']; ?>
-            <br/><span style="font-size:20px;"><?= Hawk::metricsName('ga:pageviews') ?></span>
+          <p class="stats-heading"><?= $metrics['ga:pageviews']; ?>
+            <br/><span class="stats-details"><?= Hawk::metricsName('ga:pageviews') ?></span>
           </p>
 
-          <p style="line-height: 30px; margin-bottom:40px;"><?= number_format($metrics['ga:bounceRate'],2 ); ?>%
-            <br/><span style="font-size:20px;"><?= Hawk::metricsName('ga:bounceRate') ?></span>
+          <p class="stats-heading"><?= number_format($metrics['ga:bounceRate'],2 ); ?>%
+            <br/><span class="stats-details"><?= Hawk::metricsName('ga:bounceRate') ?></span>
           </p>
 
-          <p style="line-height: 30px; margin-bottom:40px;"><?= number_format($metrics['ga:avgPageLoadTime'], 2); ?>s
-            <br/><span style="font-size:20px;"><?= Hawk::metricsName('ga:avgPageLoadTime') ?></span>
+          <p class="stats-heading"><?= number_format($metrics['ga:avgPageLoadTime'], 2); ?>s
+            <br/><span class="stats-details"><?= Hawk::metricsName('ga:avgPageLoadTime') ?></span>
           </p>
 
         <?php endif ?>
